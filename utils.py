ab6664fdccbd5895fd81830127726ad5e8f614f7
Azraele <dconor.post@gmail.com>
2022-06-15 22:33:32 +0300

    Added tasks

---
+++ utils.py
@@ -0,0 +1,16 @@
+# # ToDo:
+# #   Функция, засекающая время действия, находящаяся внутри класса timer
+# #   Генерация HTML-документа, как функция html_gen принимает аргументы testid, descriiption, precondition
+# #   Друг за другом в приличном виде дохуя
+# # TestID : ID \\ get_test_id
+# # Description: \\ get_descr
+# # бла-бла
+# # бла
+# #
+# # precondition: текст, pass\false + {precondition_time}
+# # (step, expected result, pass\false) в виде таблицы
+#
+# #   действия выполненные после самого теста и время, pass\false, time=)
+# результаты всех функций записываются в разные переменные в конструкторе класса (self)
+
+# конструктор класса => переменные => HTML
