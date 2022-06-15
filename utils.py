1391213706714d8a89ef06f77ea5139957c6e4c2
Azraele <dconor.post@gmail.com>
2022-06-15 22:59:11 +0300

    Added timer function

--- utils.py
+++ utils.py
@@ -2,15 +2,21 @@
 # #   Функция, засекающая время действия, находящаяся внутри класса timer
 # #   Генерация HTML-документа, как функция html_gen принимает аргументы testid, descriiption, precondition
 # #   Друг за другом в приличном виде дохуя
-# # TestID : ID \\ get_test_id
-# # Description: \\ get_descr
-# # бла-бла
-# # бла
-# #
-# # precondition: текст, pass\false + {precondition_time}
-# # (step, expected result, pass\false) в виде таблицы
-#
-# #   действия выполненные после самого теста и время, pass\false, time=)
-# результаты всех функций записываются в разные переменные в конструкторе класса (self)
+# #     TestID : ID \\ get_test_id
+# #     Description: \\ get_descr
+# #     бла-бла
+# #     бла
+# #     precondition: текст, pass\false + {precondition_time}
+#       (step, expected result, pass\false) в виде таблицы
+#       действия выполненные после самого теста и время, pass\false, time=)
+#       ============
+#       результаты всех функций записываются в разные переменные в конструкторе класса (self)
+#       Конструктор класса => переменные => HTML
 
-# конструктор класса => переменные => HTML
+import time
+
+
+def timer(any_func):
+    start = time.time()
+    any_func()
+    return time.time() - start
